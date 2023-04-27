4e2224da1dcde7f8a7ce961d9fb5e4466eee41ea
anubhav6488 <anubhav6488@gmail.com>
2023-04-27 16:01:47 +0530

    firebase

--- connections/firebase.js
+++ connections/firebase.js
@@ -2,16 +2,21 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGSf4WTJdh8nq8jmFncVTOGO5cMkHrvBA",
-  authDomain: "of-experiences.firebaseapp.com",
-  databaseURL: "https://of-experiences-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "of-experiences",
-  storageBucket: "of-experiences.appspot.com",
-  messagingSenderId: "184218624803",
-  appId: "1:184218624803:web:2716592e53d32ccad9e296"
+  apiKey: "AIzaSyAXQjvmCglHyQYzBxyGMA46IdbKHBcIxeo",
+  authDomain: "gramify-anubhav.firebaseapp.com",
+  projectId: "gramify-anubhav",
+  storageBucket: "gramify-anubhav.appspot.com",
+  messagingSenderId: "556809714050",
+  appId: "1:556809714050:web:908e2aa3c271089c24779b",
+  // apiKey: "AIzaSyAxnjZk50PH1szrAUrOQkB0X-K5s-JL1PM",
+  // authDomain: "staging-ofex.firebaseapp.com",
+  // projectId: "staging-ofex",
+  // storageBucket: "staging-ofex.appspot.com",
+  // messagingSenderId: "11892327779",
+  // appId: "1:11892327779:web:81e1e0c56b4d225d5aff56"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
 
 module.exports = db;
